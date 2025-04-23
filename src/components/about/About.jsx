de5f453578dd886364ec4bc71f9f3d9f10a46809
ParagKhankari <paragkhankari2345@gmail.com>
2025-04-23 12:28:49 -0400

    Updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
           <p>
   Hey! I’m a data-driven problem solver with 4+ years of experience as a Data Analyst and Business Data Analyst, turning complex datasets into clear, actionable insights across the healthcare and financial services industries.<br />
   I’ve always been fascinated by how data can drive smarter decisions and I genuinely enjoy collaborating with stakeholders to understand what they need and turn that into scalable, data-driven solutions.<br />
-  At <strong>Accenture</strong>, I worked on financial analytics building fraud detection models, ensuring data privacy with GDPR and PCI-DSS, and creating dashboards for real time insights.<br />
+  At <strong>EY</strong>, I worked on financial analytics building fraud detection models, ensuring data privacy with GDPR and PCI-DSS, and creating dashboards for real time insights.<br />
   At <strong>Cardinal Health</strong>, I lead healthcare analytics automating ETL pipelines, building predictive models, and developing interactive dashboards in Power BI and Tableau.<br />
   I work with tools like <strong>SQL</strong> (PostgreSQL, BigQuery), <strong>Python</strong> (Pandas, Airflow), <strong>Power BI</strong>, <strong>Tableau</strong>, <strong>Spark</strong>, <strong>Docker</strong>, <strong>Kubernetes</strong>, and more always aiming to deliver clean, scalable solutions.<br />
   I love collaborating with multiple stakeholders, asking the right questions, and staying curious about what matters most to them because that’s where real impact begins.<br />
